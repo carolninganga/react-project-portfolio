44f52bcfdc53b06429ad68547a5e96eed296f844
Caroline Ninganga <carolninganga@gmail.com>
2020-07-08 21:16:43 -0400

    added links to cards

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -60,13 +60,13 @@ const cards = [
     linkDeployed: "https://she-sparkles-chat-app.herokuapp.com/",
   },
   {
-    title: "6",
+    title: "Note Taker",
     description:
-      "YesReply uses two APIs Words API and Linguatools to facilitate message generation for “life events”.",
+      "A note taking application that uses allows the user to create, update and delete saved notes.",
     tileImg:
-      "url(https://cdn.glitch.com/823d971b-2ee5-4e17-bb98-ec24b973c4c0%2FScreen%20Shot%202020-07-06%20at%2010.48.44%20PM.png?v=1594090135096) center / cover",
-    linkGitHub: "https://github.com/carolninganga/Yes-Reply-Project-One",
-    linkDeployed: "https://carolninganga.github.io/Yes-Reply-Project-One/",
+      "url(https://cdn.glitch.com/823d971b-2ee5-4e17-bb98-ec24b973c4c0%2FScreen%20Shot%202020-06-26%20at%202.03.34%20PM.png?v=1593194643900) center / cover",
+    linkGitHub: "https://github.com/carolninganga/note-taker-nodejs",
+    linkDeployed: "https://note-take-nodejs.herokuapp.com/",
   },
 ];
 
@@ -77,67 +77,6 @@ class Portfolio extends Component {
     cards: cards,
     display: [],
   };
-
-  // toggleCategories() {
-  //   
-  //         <Cell col={4}>
-  //           <Card shadow={5}>
-  //             <CardTitle
-  //               style={{
-  //                 color: "#fff",
-  //                 height: "176px",
-  //                 background:
-  //                   "url(https://cdn.glitch.com/823d971b-2ee5-4e17-bb98-ec24b973c4c0%2FScreen%20Shot%202020-07-06%20at%2010.42.38%20PM.png?v=1594089782545) center / cover",
-  //               }}
-  //             ></CardTitle>
-  //             <CardText>
-  //               <h5>Day Planner</h5>
-  //               Calendar Application that allows the user to save events for
-  //               each hour of the day. This app uses moment library.
-  //             </CardText>
-  //             <CardActions border>
-  //               <Button
-  //                 target='_blank'
-  //                 rel='noopener noreferrer'
-  //                 href='https://github.com/carolninganga/Day_Planner'
-  //               >
-  //                 GitHub
-  //               </Button>
-  //               <Button
-  //                 target='_blank'
-  //                 rel='noopener noreferrer'
-  //                 href='https://carolninganga.github.io/Day_Planner/'
-  //               >
-  //                 Link to site
-  //               </Button>
-  //             </CardActions>
-  //             <CardMenu style={{ color: "#fff" }}>
-  //               <IconButton name='share' />
-  //             </CardMenu>
-  //           </Card>
-  //         </Cell>
-  //       </>
-  //     );
-  //   } else if (this.state.activeTab === 1) {
-  //     return (
-  //       <div>
-  //         <h1>This is Angular</h1>
-  //       </div>
-  //     );
-  //   } else if (this.state.activeTab === 2) {
-  //     return (
-  //       <div>
-  //         <h1>This is VueJS</h1>
-  //       </div>
-  //     );
-  //   } else if (this.state.activeTab === 3) {
-  //     return (
-  //       <div>
-  //         <h1>This is MongoDB</h1>
-  //       </div>
-  //     );
-  //   }
-  // }
 
   componentDidMount() {
     this.updateDisplay();
@@ -179,7 +118,6 @@ class Portfolio extends Component {
   render() {
     return (
       <div>
-        {/* <Grid>{this.toggleCategories()}</Grid> */}
         <button onClick={this.reversePagination}> back </button>
         <button onClick={this.advancePagination}>forward </button>
         <Grid>
