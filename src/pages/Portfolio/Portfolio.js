a5179747b3e04c0a18b0f3766b1e527056c77eb8
Caroline Ninganga <carolineninganga@Carolines-MBP.home>
2020-07-12 22:00:15 -0400

    fixed card links

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,15 +1,14 @@
-
 import React from 'react';
 import ProjectCard from '../../components/ProjectCard/ProjectCard';
-
-
-
+const gridContainerStyle = {
+  display: "grid",
+  gridTemplateColumns: "auto auto auto",
+  padding: "10px",
+  gridGap: "3rem"
+}
 export default function Portfolio() {
-
   return (
-    <div className="container">
-          <div className="row">
-            <div className="col-md-4 box">
+    <div className="container" style={gridContainerStyle}>
                 <ProjectCard 
                     img="https://cdn.glitch.com/823d971b-2ee5-4e17-bb98-ec24b973c4c0%2FScreen%20Shot%202020-07-06%20at%2010.48.44%20PM.png?v=1594090135096"
                     name="Yes Reply"
@@ -17,8 +16,6 @@ export default function Portfolio() {
                     github="https://github.com/carolninganga/Yes-Reply-Project-One"
                     site="https://carolninganga.github.io/Yes-Reply-Project-One/"
                              />
-            </div>
-            <div className="col-md-4 box">
                 <ProjectCard 
                     img="https://cdn.glitch.com/823d971b-2ee5-4e17-bb98-ec24b973c4c0%2FScreen%20Shot%202020-07-06%20at%2010.34.52%20PM.png?v=1594089329409"
                     name="Super Github Finder"
@@ -26,8 +23,6 @@ export default function Portfolio() {
                     github="https://github.com/carolninganga/react-github-finder"
                     site="https://github-repo-finder23.herokuapp.com/"
                       />
-            </div>
-            <div className="col-md-4 box">
                 <ProjectCard 
                     img="https://cdn.glitch.com/823d971b-2ee5-4e17-bb98-ec24b973c4c0%2FScreen%20Shot%202020-07-08%20at%207.27.06%20PM.png?v=1594250868304"
                     name="Fantastic Joke App"
@@ -35,20 +30,13 @@ export default function Portfolio() {
                     github="https://github.com/carolninganga/joke-app"
                     site="https://joke-master007.herokuapp.com/"
                       />
-            </div>
-            </div>
-            
-          <div className="row">
-            <div className="col-md-4 box">
                 <ProjectCard 
-                    img="https://cdn.glitch.com/823d971b-2ee5-4e17-bb98-ec24b973c4c0%2FScreen%20Shot%202020-07-08%20at%208.43.32%20PM.png?v=1594255434301"
-                    name="Hourly Calender Notepad"
-                    description="Calendar Application that uses moment to display a calendar and allows user to save notes of different hours during the day."
-                    github="https://github.com/carolninganga/Day_Planner"
-                    site="https://carolninganga.github.io/Day_Planner/"
+                    img="https://cdn.glitch.com/823d971b-2ee5-4e17-bb98-ec24b973c4c0%2Ffoodie.jpg?v=1594604861129"
+                    name="Food Me"
+                    description="A small application that uses the Yelp API and react to search for local food spots in the area."
+                    github="https://github.com/carolninganga/foodie-me"
+                    site="https://foodie-me.herokuapp.com/"
                     />
-            </div>
-            <div className="col-md-4 box">
                 <ProjectCard 
                     img="https://cdn.glitch.com/823d971b-2ee5-4e17-bb98-ec24b973c4c0%2FScreen%20Shot%202020-06-26%20at%202.03.34%20PM.png?v=1593194643900"
                     name="Note Taker"
@@ -56,22 +44,12 @@ export default function Portfolio() {
                     github="https://github.com/carolninganga/note-taker-nodejs"
                     site="https://note-take-nodejs.herokuapp.com/"
                 />
-            </div>
-            <div className="col-md-4 box">
                 <ProjectCard 
                     img="https://cdn.glitch.com/823d971b-2ee5-4e17-bb98-ec24b973c4c0%2FScreen%20Shot%202020-07-08%20at%209.00.05%20PM.png?v=159425643256"
                     name="She Sparkles Chat"
-                    description="She Sparkles is a chat application that allows users to enter different rooms to chat. Tools include HTML, JavaScript and CSS."
+                    description="She Sparkles is a chat application that allows users to enter different rooms to chat."
                     github="https://github.com/carolninganga/she-sprakles-chat-application"
                     site="https://she-sparkles-chat-app.herokuapp.com/"/>
-            </div>
-          </div>
     </div>
-   
   );
 }
-
-
-
-
-
